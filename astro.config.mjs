b42ed22fc25dc0b8d678576c7a9d1c98397828b6
Oliver Pirger <oliver@pirger.eu>
2025-08-19 11:29:05 +0200

    font

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,27 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  experimental: {
+    fonts: [
+      {
+        cssVariable: "--font-inter",
+        name: "Inter",
+        provider: "local",
+        variants: [
+          {
+            src: ["./src/assets/fonts/InterVariable.woff2"],
+            featureSettings: "'liga' 1, 'calt' 1",
+            style: "normal",
+          },
+          {
+            src: ["./src/assets/fonts/InterVariable-Italic.woff2"],
+            featureSettings: "'liga' 1, 'calt' 1",
+            style: "italic",
+          },
+        ],
+      },
+    ],
+  },
+});
