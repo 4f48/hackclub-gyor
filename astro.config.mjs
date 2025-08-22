fbd12699a94ca2b7fba81154097db96929f6568d
Oliver Pirger <oliver@pirger.eu>
2025-08-22 17:56:17 +0200

    registration form

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,23 +8,6 @@ export default defineConfig({
   experimental: {
     fonts: [
       {
-        cssVariable: "--font-inter",
-        name: "Inter",
-        provider: "local",
-        variants: [
-          {
-            src: ["./src/assets/fonts/InterVariable.woff2"],
-            featureSettings: "'liga' 1, 'calt' 1",
-            style: "normal",
-          },
-          {
-            src: ["./src/assets/fonts/InterVariable-Italic.woff2"],
-            featureSettings: "'liga' 1, 'calt' 1",
-            style: "italic",
-          },
-        ],
-      },
-      {
         cssVariable: "--font-jbm",
         name: "JetBrains Mono",
         provider: "local",
