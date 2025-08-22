5e687333acfc30e46236c15d63ee2704955dc842
Oliver Pirger <oliver@pirger.eu>
2025-08-22 23:32:28 +0200

    genius error handling

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -17,58 +17,63 @@ export const server = {
       birthday: z.string().date(),
     }),
     handler: async (input) => {
-      const { rowsAffected } = await db.insert(member).values({
-        birthday: new Date(input.birthday),
-        discord: input.discord,
-        email: input.email,
-        name: input.name,
-        school: input.school,
-      });
-      if (rowsAffected === 1) {
-        const payload = {
-          embeds: [
-            {
-              title: "Új klubtag",
-              fields: [
-                {
-                  name: "Név",
-                  value: input.name,
-                  inline: true,
-                },
-                {
-                  name: "E-mail cím",
-                  value: input.email,
-                  inline: true,
-                },
-                {
-                  name: "Discord",
-                  value: input.discord,
-                  inline: true,
-                },
-                {
-                  name: "Iskola",
-                  value: input.school,
-                  inline: true,
-                },
-                {
-                  name: "Születésnap",
-                  value: input.birthday,
-                  inline: true,
-                },
-              ],
-              color: 0x00ff00,
-              timestamp: now("Europe/Budapest").toAbsoluteString(),
-            },
-          ],
-          username: "Orpheo",
-          avatarURL:
-            "https://rawr.hackclub.com/dinosaur_sealing_letters_with_wax.png",
-        };
-        await fetch(import.meta.env.DISCORD_WEBHOOK_URL, {
-          method: "post",
-          headers: { "content-type": "application/json" },
-          body: JSON.stringify(payload),
+      try {
+        const { rowsAffected } = await db.insert(member).values({
+          birthday: new Date(input.birthday),
+          discord: input.discord,
+          email: input.email,
+          name: input.name,
+          school: input.school,
         });
+        if (rowsAffected === 1) {
+          const payload = {
+            embeds: [
+              {
+                title: "Új klubtag",
+                fields: [
+                  {
+                    name: "Név",
+                    value: input.name,
+                    inline: true,
+                  },
+                  {
+                    name: "E-mail cím",
+                    value: input.email,
+                    inline: true,
+                  },
+                  {
+                    name: "Discord",
+                    value: input.discord,
+                    inline: true,
+                  },
+                  {
+                    name: "Iskola",
+                    value: input.school,
+                    inline: true,
+                  },
+                  {
+                    name: "Születésnap",
+                    value: input.birthday,
+                    inline: true,
+                  },
+                ],
+                color: 0x00ff00,
+                timestamp: now("Europe/Budapest").toAbsoluteString(),
+              },
+            ],
+            username: "Orpheo",
+            avatarURL:
+              "https://rawr.hackclub.com/dinosaur_sealing_letters_with_wax.png",
+          };
+          await fetch(import.meta.env.DISCORD_WEBHOOK_URL, {
+            method: "post",
+            headers: { "content-type": "application/json" },
+            body: JSON.stringify(payload),
+          });
+        }
+      } catch (err) {
+        console.error(err);
+        return new Response("something went wronk", { status: 500 });
       }
     },
   }),
