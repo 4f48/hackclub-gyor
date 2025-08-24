e4209898a8a627c04f33feef5182f3e22adefc05
Oliver Pirger <oliver@pirger.eu>
2025-08-24 14:04:06 +0200

    html email

--- src/actions/signup.ts
+++ src/actions/signup.ts
@@ -28,7 +28,64 @@ export default defineAction({
         from: "Hack Club Győr <noreply@mail.hackclubgyor.com>",
         to: input.email,
         subject: "Regisztráció befejezése",
-        text: generateOauthLink(id),
+        html: `
+          <!DOCTYPE html>
+          <html>
+            <head>
+              <link rel="preconnect" href="https://fonts.googleapis.com" />
+              <link
+                rel="preconnect"
+                href="https://fonts.gstatic.com"
+                crossorigin
+              />
+              <link
+                href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,100..800;1,100..800&display=swap"
+                rel="stylesheet"
+              />
+              <style>
+                body {
+                  font-family: "JetBrains Mono", monospace;
+                  font-optical-sizing: auto;
+                  font-style: normal;
+
+                  background-color: black;
+                  color: white;
+                }
+                h1 {
+                  color: #00ff00;
+                  font-weight: 800;
+                  font-size: 1.5rem;
+                  line-height: 2rem;
+                  margin-bottom: 15px;
+                }
+                p {
+                  text-align: justify;
+                }
+                a {
+                  color: #00ff00;
+                  padding-left: 0.25rem;
+                  padding-right: 0.25rem;
+                  text-decoration: none;
+                }
+                a:hover {
+                  color: black;
+                  background-color: #00ff00;
+                }
+              </style>
+            </head>
+            <body>
+              <h1>Hack Club Győr</h1>
+              <p>Kedves ${input.name}!</p>
+              <p>
+                Megkaptuk a jelentkezésedet a győri Hack Club közösségébe.
+                Ahhoz, hogy teljeskörű klubtag lehess, be kell lépj Discord
+                szerverünkre. Az alábbi linkre kattintva összekapcsolhatod a
+                jelentkezésed Discord fiókoddal.
+              </p>
+              <a href="${generateOauthLink(id)}">Discord bejelentkezés &gt;</a>
+            </body>
+          </html>
+        `,
       });
       if (error) {
         console.error(error);
