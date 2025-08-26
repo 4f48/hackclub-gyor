d301acb2b5e794d8583f93e0b5c582f39a687c26
Oliver Pirger <oliver@pirger.eu>
2025-08-26 10:23:59 +0200

    fix birthday box focus, error message

--- src/components/birthday.svelte
+++ src/components/birthday.svelte
@@ -6,7 +6,11 @@
 <DateField.Root locale="hu" required>
 	<div class="mb-4 grid focus-within:text-[#00ff00]">
 		<DateField.Label>Születésnap</DateField.Label>
-		<DateField.Input class="border p-1" id="birthday" name="birthday">
+		<DateField.Input
+			class="p-1 outline focus-within:outline-2 focus-within:outline-[#00ff00]"
+			id="birthday"
+			name="birthday"
+		>
 			{#snippet children({ segments })}
 				{#each segments as { part, value }}
 					<DateField.Segment
